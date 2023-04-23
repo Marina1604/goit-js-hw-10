67b86a4b2d1b2d5dd9781a98dcc5241c3753c451
Marina1604 <117771855+Marina1604@users.noreply.github.com>
2023-04-23 20:24:21 +0300

    Y

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,4 @@
+
 const BASE_URL = 'https://restcountries.com/v3.1/name/'
 const fields = 'fields=name,capital,population,flags,languages'
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/styles.css';
 import debounce from 'lodash.debounce'
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
+
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
